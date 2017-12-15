52dc90c60d5da0b0b8a2a0d7faf1f11979b4ec52
David <davvvvvvidchen@gmail.com>
2017-12-15 16:22:56 +0800

    start spring cloud config server.

---
+++ src/main/java/com/cbcing/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.cbcing.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 构建Spring Cloud Config : 分布式配置中心
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+}
