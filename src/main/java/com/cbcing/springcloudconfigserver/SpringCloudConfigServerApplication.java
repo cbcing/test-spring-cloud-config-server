cc4d40a19fe61be818bfca9636005f293c60e86e
David <davvvvvvidchen@gmail.com>
2017-12-20 17:07:23 +0800

    增加此服务的注册（目前无效）

--- src/main/java/com/cbcing/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ src/main/java/com/cbcing/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,12 +2,14 @@ package com.cbcing.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * 构建Spring Cloud Config : 分布式配置中心
  */
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
